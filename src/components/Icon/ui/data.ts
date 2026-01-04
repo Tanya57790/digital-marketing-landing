7af938a812022234f50af5cc2e9fcdfdc38d5f8d
Tatyana Makhortova <tmahortova67@gmail.com>
2026-01-04 23:44:04 +0500

    feat(data): add data.ts for Icon component

---
+++ src/components/Icon/ui/data.ts
@@ -0,0 +1,44 @@
+export const heroData = [
+  {
+    id: 1,
+    hero: '/assets/icons/lev-haolam.svg',
+    mobile: { width: '70.23px', height: '26.03px' },
+    desktop: { width: '180px', height: '66.72px' },
+  },
+  {
+    id: 2,
+    hero: '/assets/icons/schneider-electric.svg',
+    mobile: { width: '70.23px', height: '20.72px' },
+    desktop: { width: '180px', height: '53.1px' },
+  },
+  {
+    id: 3,
+    hero: '/assets/icons/aliexpress.svg',
+    mobile: { width: '70.23px', height: '16.33px' },
+    desktop: { width: '180px', height: '41.85px' },
+  },
+  {
+    id: 4,
+    hero: '/assets/icons/la-roche-posay.svg',
+    mobile: { width: '70.23px', height: '30.02px' },
+    desktop: { width: '180px', height: '76.95px' },
+  },
+  {
+    id: 5,
+    hero: '/assets/icons/papajohns.svg',
+    mobile: { width: '70.23px', height: '12.99px' },
+    desktop: { width: '180px', height: '33.3px' },
+  },
+  {
+    id: 6,
+    hero: '/assets/icons/kfc.svg',
+    mobile: { width: '70.23px', height: '20.89px' },
+    desktop: { width: '180px', height: '53.55px' },
+  },
+];
+
+export const footerData = [
+  { id: 1, footer: '/assets/icons/twitter.svg', size: '40px' },
+  { id: 2, footer: '/assets/icons/instagram.svg', size: '40px' },
+  { id: 3, footer: '/assets/icons/facebook.svg', size: '40px' },
+];
