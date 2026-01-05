1a5f9e58c6c26dd378e74b3f45cdfc7cbf5c503d
Tatyana Makhortova <tmahortova67@gmail.com>
2026-01-06 00:32:50 +0500

    refactor(data): replace object keys 'hero', 'footer' with 'src'

--- src/components/Icon/ui/data.ts
+++ src/components/Icon/ui/data.ts
@@ -1,7 +1,7 @@
 export const heroData = [
   {
     id: 1,
-    hero: '/assets/icons/lev-haolam.svg',
+    src: '/assets/icons/lev-haolam.svg',
     mobile: { width: '70.23px', height: '26.03px' },
     tablet: { width: '100px', height: '38px' },
     laptop: { width: '130px', height: '49px' },
@@ -9,7 +9,7 @@ export const heroData = [
   },
   {
     id: 2,
-    hero: '/assets/icons/schneider-electric.svg',
+    src: '/assets/icons/schneider-electric.svg',
     mobile: { width: '70.23px', height: '20.72px' },
     tablet: { width: '100px', height: '30px' },
     laptop: { width: '130px', height: '38px' },
@@ -17,7 +17,7 @@ export const heroData = [
   },
   {
     id: 3,
-    hero: '/assets/icons/aliexpress.svg',
+    src: '/assets/icons/aliexpress.svg',
     mobile: { width: '70.23px', height: '16.33px' },
     tablet: { width: '100px', height: '24px' },
     laptop: { width: '130px', height: '31px' },
@@ -25,7 +25,7 @@ export const heroData = [
   },
   {
     id: 4,
-    hero: '/assets/icons/la-roche-posay.svg',
+    src: '/assets/icons/la-roche-posay.svg',
     mobile: { width: '70.23px', height: '30.02px' },
     tablet: { width: '100px', height: '44px' },
     laptop: { width: '130px', height: '57px' },
@@ -33,7 +33,7 @@ export const heroData = [
   },
   {
     id: 5,
-    hero: '/assets/icons/papajohns.svg',
+    src: '/assets/icons/papajohns.svg',
     mobile: { width: '70.23px', height: '12.99px' },
     tablet: { width: '100px', height: '18px' },
     laptop: { width: '130px', height: '24px' },
@@ -41,7 +41,7 @@ export const heroData = [
   },
   {
     id: 6,
-    hero: '/assets/icons/kfc.svg',
+    src: '/assets/icons/kfc.svg',
     mobile: { width: '70.23px', height: '20.89px' },
     tablet: { width: '100px', height: '30px' },
     laptop: { width: '130px', height: '38px' },
@@ -50,7 +50,7 @@ export const heroData = [
 ];
 
 export const footerData = [
-  { id: 1, footer: '/assets/icons/twitter.svg', size: '40px' },
-  { id: 2, footer: '/assets/icons/instagram.svg', size: '40px' },
-  { id: 3, footer: '/assets/icons/facebook.svg', size: '40px' },
+  { id: 1, src: '/assets/icons/twitter.svg', size: '40px' },
+  { id: 2, src: '/assets/icons/instagram.svg', size: '40px' },
+  { id: 3, src: '/assets/icons/facebook.svg', size: '40px' },
 ];
