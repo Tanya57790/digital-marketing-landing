2209e9e300e1068962de5def3b9ed8b3ad9eeab3
Tatyana Makhortova <tmahortova67@gmail.com>
2026-01-10 00:36:04 +0500

    feat(data): add data.ts for Menu and BurgerMenu components

---
+++ src/components/Menu/ui/data.ts
@@ -0,0 +1,11 @@
+interface ItemProps {
+  id: number;
+  name: string;
+}
+
+export const menuItem: ItemProps[] = [
+  { id: 1, name: 'Services' },
+  { id: 2, name: 'Portfolio' },
+  { id: 3, name: 'Email Marketing Audit' },
+  { id: 4, name: 'Contact us' },
+];
