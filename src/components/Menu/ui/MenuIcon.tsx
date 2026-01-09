42483330bebfe7817d76ae02ddb9986295c68b3e
Tatyana Makhortova <tmahortova67@gmail.com>
2026-01-10 00:39:22 +0500

    feat(icon): add MenuIcon for Menu and BurgerMenu components

---
+++ src/components/Menu/ui/MenuIcon.tsx
@@ -0,0 +1,69 @@
+'use client';
+
+import { styled, useTheme } from '@mui/material';
+
+interface MenuIconProps {
+  variant: 'burger' | 'cross';
+  onClick?: () => void;
+}
+
+const StyledIcon = styled('svg')(({ theme }) => ({
+  cursor: 'pointer',
+  position: 'absolute',
+  right: 0,
+  padding: theme.spacing(0, 2, 0, 0),
+}));
+
+const MenuIcon = ({ variant, onClick }: MenuIconProps) => {
+  const theme = useTheme();
+
+  return (
+    <>
+      {variant === 'burger' && (
+        <StyledIcon
+          onClick={onClick}
+          width="25"
+          height="16"
+          viewBox="0 0 25 16"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          role="img"
+        >
+          <rect width="25" height="2" rx="1" fill={theme.palette.primary.main} />
+          <rect y="7" width="25" height="2" rx="1" fill={theme.palette.primary.main} />
+          <rect y="14" width="25" height="2" rx="1" fill={theme.palette.primary.main} />
+        </StyledIcon>
+      )}
+      {variant === 'cross' && (
+        <StyledIcon
+          sx={{ top: '20px' }}
+          onClick={onClick}
+          width="25"
+          height="16"
+          viewBox="0 0 25 16"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          role="img"
+        >
+          <rect
+            width="25"
+            height="2"
+            rx="1"
+            transform="rotate(30)"
+            fill={theme.palette.primary.contrastText}
+          />
+          <rect
+            y="7"
+            width="25"
+            height="2"
+            rx="1"
+            transform="rotate(330, 10, 10)"
+            fill={theme.palette.primary.contrastText}
+          />
+        </StyledIcon>
+      )}
+    </>
+  );
+};
+
+export default MenuIcon;
