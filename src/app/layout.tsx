aad03d1b773044b13ecc3401f3511b47f0556db4
Tatyana Makhortova <tmahortova67@gmail.com>
2026-01-13 21:47:56 +0500

    refactor(layout): wrap content in Container with responsive padding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v16-appRouter';
 import { ThemeProvider, CssBaseline, Container } from '@mui/material';
 import { theme } from '@/theme';
+import '@fontsource/roboto-condensed/700.css';
 
 export const metadata: Metadata = {
   title: 'Boost your email marketing results!',
@@ -10,12 +11,17 @@ export const metadata: Metadata = {
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <html lang="en" className="dark">
+    <html lang="en">
       <body>
         <AppRouterCacheProvider options={{ enableCssLayer: false }}>
           <ThemeProvider theme={theme}>
             <CssBaseline />
-            <Container maxWidth="xl">
+            <Container
+              maxWidth="desktop"
+              sx={{
+                padding: { xs: '0px 20px', md: '0px 30px', lg: '0px 50px', desktop: '0px 60px' },
+              }}
+            >
               <main>{children}</main>
             </Container>
           </ThemeProvider>
