fa1462c46cd0531fdd9c8a1f7781bb6b7da5db8f
Tatyana Makhortova <tmahortova67@gmail.com>
2026-01-19 02:04:38 +0500

    feat(ui): add Input component

---
+++ src/components/Input/ui/Input.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import { styled } from '@mui/material';
+
+const StyledInput = styled('input')(({ theme }) => ({
+  width: '143px',
+  border: 'none',
+  borderTopLeftRadius: '5px',
+  borderBottomLeftRadius: '5px',
+  paddingLeft: '10px',
+  fontSize: '14px',
+  fontFamily: '"Roboto", "Arial", sans-serif',
+  fontWeight: 300,
+  [theme.breakpoints.up('desktop')]: {
+    width: '732px',
+    paddingLeft: '30px',
+    fontSize: '20px',
+  },
+  ':focus': {
+    outline: 'none',
+  },
+}));
+
+const Input = () => {
+  return (
+    <StyledInput
+      id="email"
+      name="email"
+      placeholder="Your Email"
+      type="email"
+      max-length="100"
+      required
+    />
+  );
+};
+
+export default Input;
