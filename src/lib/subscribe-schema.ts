aae4f01d32daceb729f289219c1888dd69655542
Tatyana Makhortova <tmahortova67@gmail.com>
2026-01-20 23:53:27 +0500

    feat(lib): add subscribe schema to Form component

---
+++ src/lib/subscribe-schema.ts
@@ -0,0 +1,11 @@
+import * as z from 'zod';
+
+export const schema = z.object({
+  email: z
+    .string()
+    .max(320, 'Maximum length is 320 characters')
+    .min(1, 'Required field')
+    .refine((email) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email), {
+      message: 'Email is not valid',
+    }),
+});
