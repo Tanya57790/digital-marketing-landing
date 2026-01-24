4cf49556a3a58c1357f1ea574cb3ee2bdeb168cc
Tatyana Makhortova <tmahortova67@gmail.com>
2026-01-24 22:26:04 +0500

    feat(api): add subscription api route

---
+++ src/app/api/subscription/route.ts
@@ -0,0 +1,21 @@
+'use server';
+
+import { schema, type InferSchema } from '@/lib/subscribe-schema';
+import z, { ZodError } from 'zod';
+import { prisma } from '@/lib/prisma';
+
+export async function addSubscriber(formData: InferSchema) {
+  const result = schema.safeParse(formData);
+  if (!result.success) {
+    return {
+      errors: z.treeifyError(result.error as ZodError),
+    };
+  }
+  const subscriber = await prisma.subscriber.upsert({
+    where: { email: result.data.email },
+    update: {},
+    create: { email: result.data.email },
+  });
+
+  return { success: true, subscriberId: subscriber.id, message: 'Validation successful' };
+}
