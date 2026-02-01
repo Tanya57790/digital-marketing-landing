f5c57df3b1b012096a256817e62ac327bf3f3fc7
Tatyana Makhortova <tmahortova67@gmail.com>
2026-02-02 00:27:55 +0500

    feat(ui): add PortfolioButton component

---
+++ src/components/PortfolioButton/index.ts
@@ -0,0 +1,1 @@
+export { default as PortfolioButton } from './ui/PortfolioButton';
---
+++ src/components/PortfolioButton/ui/PortfolioButton.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import { styled } from '@mui/material';
+
+type ButtonType = {
+  onClick: () => void;
+  disabled: boolean;
+  children: React.ReactNode;
+};
+
+const StyledButton = styled('button')(({ theme }) => ({
+  border: `1px solid ${theme.palette.primary.main}`,
+  borderRadius: '100px',
+  background: theme.palette.secondary.contrastText,
+  position: 'relative',
+  width: '41px',
+  height: '41px',
+  cursor: 'pointer',
+  '&:disabled, &:disabled > span': {
+    borderColor: theme.palette.grey[500],
+    cursor: 'auto',
+  },
+  [theme.breakpoints.up('lg')]: {
+    padding: '24px',
+  },
+  [theme.breakpoints.up('xl')]: {
+    padding: '28px',
+  },
+  [theme.breakpoints.up('desktop')]: {
+    borderWidth: '2px',
+    padding: '32px',
+  },
+}));
+
+const PortfolioButton = ({ onClick, disabled, children }: ButtonType) => {
+  return (
+    <>
+      <StyledButton onClick={onClick} disabled={disabled}>
+        {children}
+      </StyledButton>
+    </>
+  );
+};
+
+export default PortfolioButton;
