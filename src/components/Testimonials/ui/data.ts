410f5a0035a1d9d822cc878b33543f37e2260028
Tatyana Makhortova <tmahortova67@gmail.com>
2026-02-06 23:40:58 +0500

    feat(data): add data for Testimonials component

---
+++ src/components/Testimonials/ui/data.ts
@@ -0,0 +1,22 @@
+export const testimonialsInfo = [
+  {
+    id: 1,
+    quote: [
+      'You have ensured\n a systematic ',
+      'approach\n to our email strategy\n and ',
+      'helped us streamline\n all chaotic thoughts\n and ',
+      'ideas we had about\n our emails!',
+    ],
+    userName: 'Natalia Pereldik',
+    profession: 'CEO, Funexpected Games',
+  },
+  {
+    id: 2,
+    quote: [
+      'You have shown great\n professionalism and\n ',
+      'attention to detail when it\n comes to email marketing!',
+    ],
+    userName: 'Ron Shalev,',
+    profession: 'Founder, BYB Media',
+  },
+];
