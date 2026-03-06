bd45e69334605710192c5368a50340c9323435ea
Tatyana Makhortova <tmahortova67@gmail.com>
2026-03-06 20:17:52 +0500

    refactor(data): add `href` for navigation to Menu data

--- src/components/Menu/ui/data.ts
+++ src/components/Menu/ui/data.ts
@@ -1,11 +1,12 @@
 interface ItemProps {
   id: number;
   name: string;
+  href: string;
 }
 
 export const menuItem: ItemProps[] = [
-  { id: 1, name: 'Services' },
-  { id: 2, name: 'Portfolio' },
-  { id: 3, name: 'Email Marketing Audit' },
-  { id: 4, name: 'Contact us' },
+  { id: 1, name: 'Services', href: '#services' },
+  { id: 2, name: 'Portfolio', href: '#portfolio' },
+  { id: 3, name: 'Email Marketing Audit', href: '#audit' },
+  { id: 4, name: 'Contact us', href: '#' },
 ];
