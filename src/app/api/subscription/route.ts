f04234b79aa5e779b86a95daa5c4d83a4cbe4a8a
Tatyana Makhortova <tmahortova67@gmail.com>
2026-04-04 22:18:29 +0500

    feat(subscription): add email newsletters
    Email newsletters for first subscribers and checking subscribers in the database.

--- src/app/api/subscription/route.ts
+++ src/app/api/subscription/route.ts
@@ -2,11 +2,62 @@
 
 import { NextRequest, NextResponse } from 'next/server';
 import { createSubscriber } from '@/lib/subscribers';
+import { transporter } from '@/lib/email';
+import { generateUnsubscribeJWT } from '@/lib/utils';
+import { prisma } from '@/lib/prisma';
 
 export async function POST(request: NextRequest) {
   try {
     const formData = await request.json();
+    const email = formData.email;
     const result = await createSubscriber(formData);
+    const unsubscribeId = generateUnsubscribeJWT(email);
+    const user = await prisma.subscriber.findUnique({
+      where: { email: email },
+    });
+
+    if (user && !user.messageSent) {
+      await transporter.sendMail({
+        from: `"${process.env.SENDER_NAME}" <${process.env.EMAIL_USER}>`,
+        to: email,
+        subject: 'Welcome!',
+        html: `
+        <h1>Thank you for subscribing!</h1>
+        <h2>Tip №1</h2>
+        <p>
+          To improve email marketing, 
+          focus on personalization, 
+          list segmentation, responsive design, 
+          and regular A/B testing of subject lines and content. 
+          Build your list legitimately, 
+          write useful emails from the perspective of real people, 
+          not brands, and optimize send times to increase open rates and click-through rates.
+        </p>
+        <a href="${request.nextUrl.origin}/api/unsubscription/${unsubscribeId}" 
+          style="background: #F5333F; 
+          display: inline-block; 
+          padding: 10px 20px; 
+          border-radius: 5px; 
+          color: white; 
+          text-decoration: none;"
+        >
+          Unsubscribe from the newsletter
+        </a>
+      `,
+      });
+
+      await prisma.subscriber.update({
+        where: { id: user.id },
+        data: { messageSent: true },
+      });
+    }
+
+    if (user && !user.subscribed) {
+      await prisma.subscriber.update({
+        where: { id: user.id },
+        data: { subscribed: true },
+      });
+    }
     return NextResponse.json(result);
   } catch (error) {
     console.error('Server error', error);
